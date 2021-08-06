4d06e6f4e96560253f7bf415f32e3e2a9b7def26
Sami063 <85618108+Sami063@users.noreply.github.com>
2021-08-06 11:05:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables and then initialize to zero.
+            int num1 = 0; int num2 = 0;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+         
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Skriv dit number, og tryk Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            // Ask the user to type the second number.
+            Console.WriteLine("Skriv dit andet number, og tryk Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Vælg en indstilling fra følgende liste:");
+            Console.WriteLine("+");
+            Console.WriteLine("-");
+            Console.WriteLine("*");
+            Console.WriteLine("/");
+            Console.Write("Your option? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "/":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            
+        }
+    }
+}
